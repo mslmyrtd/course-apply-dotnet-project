88ddf39a3ca5f162b19fb735fc06c3ca4a4b367d
mslmyrtd <mslmyrtd@gmail.com>
2024-10-14 01:25:24 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace course_apply_dotnet_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# course-apply-dotnet-project
