0458750039de9a4fd2e100e9603a018558f86b93
mslmyrtd <mslmyrtd@gmail.com>
2024-10-26 22:45:58 +0300

    added if statement for control form is valid and add unique applications

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -18,8 +18,15 @@ namespace course_apply_dotnet_project.Controllers
         [ValidateAntiForgeryToken]
          public IActionResult Apply([FromForm] Candidate model)
         {
+            if(Repository.Applications.Any(c=>c.Email.Equals(model.Email))){
+                ModelState.AddModelError("", "There is already exist application.");
+            }
+            if(ModelState.IsValid)
+            {
             Repository.Add(model);
             return View("Feedback", model);
+            }
+            return View();
         }
     }
 }
--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace course_apply_dotnet_project.Models
 {
     public class Candidate
     {
+        [Required(ErrorMessage ="E-mail is required")]
         public string Email {get;set;}=string.Empty;
-        public string FirstName {get;set;}=string.Empty;
-        public string LastName {get;set;}=string.Empty;
+        [Required(ErrorMessage ="FirstName is required")]
+
+        public String? FirstName {get;set;}=string.Empty;
+        [Required(ErrorMessage ="LastName is required")]
+
+        public String? LastName {get;set;}=string.Empty;
+        public String? FullName  => $"{FirstName} {LastName?.ToUpper()}";
         public string SelectedCourse {get;set;}=string.Empty;
         public DateTime ApplyAt {get;set;}
         public int Age {get;set;}
